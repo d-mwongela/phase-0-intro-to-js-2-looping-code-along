0cae8e9d58ed9cb0386edf1cc0f69947ece0c76f
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-24 10:34:58 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+//const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, events) {
+    let message = [];
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return message;
+  }
+
+function countDown() {
+    countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+  } 
